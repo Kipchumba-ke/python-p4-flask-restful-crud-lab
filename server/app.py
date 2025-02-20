ce98bd891a4624ef492d8a25ead1d641eb91eaa4
Kelvin Kipchumba <chogekelvin90@gmail.com>
2025-02-20 11:09:08 +0300

    RESTful create, update and delete

--- server/app.py
+++ server/app.py
@@ -46,6 +46,35 @@ class PlantByID(Resource):
     def get(self, id):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
+    
+    def patch(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+
+        for attr, value in request.json.items():
+            setattr(plant, attr, value)
+
+        db.session.add(plant)
+        db.session.commit()
+
+        plant_dict = plant.to_dict()
+
+        response = make_response(plant_dict, 200,)
+        return response
+    
+    def delete(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+
+        db.session.delete(plant)
+        db.session.commit()
+
+        message = {
+            "message" : ""
+        }
+
+        response = make_response(message, 204,)
+
+        return response
+
 
 
 api.add_resource(PlantByID, '/plants/<int:id>')
